37010a76ab80d4efe891f85d4dbfdca447cc1b15
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2021-03-06 23:06:14 +0300

    Add numbers' map

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 
 
+tk_NESW = tk.N + tk.E + tk.S + tk.W
+
+
 class FifteenGame(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.buttons = []
-        self.grid(sticky=tk.N+tk.E+tk.S+tk.W)
+        self.grid(sticky=tk_NESW)
+        self.map = [
+            0, 1, 2, None,
+            4, 5, 6, 3,
+            8, 9, 10, 7,
+            12, 13, 14, 11
+        ]
         self.create_widgets()
 
 
@@ -14,9 +22,11 @@ class FifteenGame(tk.Frame):
         self.new_button.grid(row=0, column=0, columnspan=2)
         self.quit_button = tk.Button(self, text='Exit', command=self.quit)
         self.quit_button.grid(row=0, column=2, columnspan=2)
+        self.numeric_buttons = []
         for i in range(15):
-            self.buttons.append(tk.Button(self, text='{}'.format(i + 1)))
-            self.buttons[-1].grid(row=i // 4 + 1, column=i % 4, sticky=tk.N+tk.E+tk.S+tk.W)
+            btn = tk.Button(self, text='{}'.format(i + 1), command=self.create_callback(i))
+            self.numeric_buttons.append(btn)
+        self.grid_numeric_buttons()
 
         # resizable grid
         top = self.winfo_toplevel()
@@ -25,6 +35,16 @@ class FifteenGame(tk.Frame):
         for i in range(4):
             self.rowconfigure(i + 1, weight=1)
             self.columnconfigure(i, weight=1)
+
+    def grid_numeric_buttons(self):
+        for i, n in enumerate(self.map):
+            if n is not None:
+                self.numeric_buttons[n].grid(row=i // 4 + 1, column=i % 4, sticky=tk_NESW)
+
+    def create_callback(self, n):
+        def callback():
+            print(self.map.index(n))
+        return callback
 
 
 def main():
